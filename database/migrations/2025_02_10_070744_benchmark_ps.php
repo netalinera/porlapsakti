34c27bf82dead30bf398691a6a4905eef6e78aeb
Martin Ompusunggu <martincyberxpro@gmail.com>
2025-02-10 15:37:21 +0700

    add migration benchmark_ps

---
+++ database/migrations/2025_02_10_070744_benchmark_ps.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('benchmark_ps', function(Blueprint $table){
+            $table->id();
+            $table->string('id_nama_kegiatan');
+            $table->string('nama_peserta');
+            $table->string('no_telpon_peserta');
+            $table->string('email_peserta');
+            $table->unsignedBigInteger('id_lembaga');
+            $table->string('hasil_1');
+            $table->string('hasil_2');
+            $table->string('hasil_3');
+            $table->string('hasil_4');
+            $table->string('hasil_5');
+            
+          
+            $table->foreign('id_lembaga')
+                  ->references('id')
+                  ->on('lembagas')
+                  ->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('benchmark_ps');
+    }
+};
