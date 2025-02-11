40fd8c345130f5d8c403b9444d9c9909fbd51797
Martin Ompusunggu <martincyberxpro@gmail.com>
2025-02-11 08:55:14 +0700

    add master table m_kegiatan

---
+++ database/migrations/2025_02_10_070530_m_kegiatan.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('m_kegiatan', function (Blueprint $table) {
+            $table->id(); // unsignedBigInteger
+            $table->string('nama_kegiatan');
+            $table->string('jenis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('m_kegiatan');
+    }
+};
