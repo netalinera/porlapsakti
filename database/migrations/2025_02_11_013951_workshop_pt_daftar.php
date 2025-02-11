84da0593fd4479ef1192aea7b6d439d206e6ae20
Martin Ompusunggu <martincyberxpro@gmail.com>
2025-02-11 15:15:05 +0700

    add migration workshop_pt_daftar

---
+++ database/migrations/2025_02_11_013951_workshop_pt_daftar.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('workshop_pt_daftar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_nama_kegiatan');
+            $table->string('nama_peserta');
+            $table->string('no_wa_peserta');
+            $table->string('email_peserta');
+            $table->string('kode_pt');
+            $table->string('status_pt');
+            $table->string('akreditasi');
+            $table->unsignedBigInteger('id_lembaga');
+            $table->timestamp('timestamp');
+
+            $table->foreign('id_nama_kegiatan')
+                  ->references('id')
+                  ->on('m_kegiatan')
+                  ->onDelete('cascade');
+        
+            $table->foreign('id_lembaga')
+                  ->references('id')
+                  ->on('lembagas')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('workshop_pt_daftar');
+    }
+};
