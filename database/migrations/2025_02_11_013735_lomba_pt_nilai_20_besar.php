e5555a64e70a5b59b3edf9f7236f93936d5a6f17
Martin Ompusunggu <martincyberxpro@gmail.com>
2025-02-11 15:12:59 +0700

    add migration lomba_pt_20_besar

---
+++ database/migrations/2025_02_11_013735_lomba_pt_nilai_20_besar.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('lomba_pt_nilai_20_besar', function (Blueprint $table) {
+            $table->string('UniqueID')->primary();
+            $table->string('nomor_pendaftaran');
+            $table->unsignedBigInteger('id_lembaga');
+            $table->string('kode_provinsi', 10);
+            $table->string('karya_inovasi_tertulis');
+            $table->string('link_unggah_video');
+            $table->decimal('nilai_tahap_daring');
+            $table->timestamp('timestamp');
+
+            $table->foreign('kode_provinsi')
+                  ->references('kode_prov')
+                  ->on('provinsis')
+                  ->onDelete('cascade');
+
+            $table->foreign('id_lembaga')
+                  ->references('id')
+                  ->on('lembagas')
+                  ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('lomba_pt_nilai_20_besar');
+    }
+};
